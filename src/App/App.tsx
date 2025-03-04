08a7736e8953442f5a294e69521818495e1a2253
Biraj Rajbhandari <birajr0@gmail.com>
2025-03-04 22:39:27 +0545

    fix: fix eslint issue
    - change eslint version to old version

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,9 @@
-import { Button } from "../components/ui/button";
+import { Button } from '../components/ui/button';
 
 function App() {
-
   return (
-    <>
-      <Button>Button</Button>
-    </>
-  )
+    <Button>Button</Button>
+  );
 }
 
-export default App
+export default App;
